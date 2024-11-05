f1dc4d26becf1abdfce51784136cbf73c1494283
K-sesha <20103952@tafe.wa.edu.au>
2024-11-05 12:39:36 +0800

    api, gallery, route

--- app/gallery/[id]/page.js
+++ app/gallery/[id]/page.js
@@ -3,6 +3,7 @@ import {notFound} from 'next/navigation';
 import Image from 'next/image';
 import {use} from 'react';
 import { sculptureList } from "../../data/list.js";
+export const runtime = "edge";
 
 export default function GalleryItem({params}){
   // get the param from the url
